a5b99956b4bef6262344eac1c115b54ec7ac6495
bakano98 <70756909+bakano98@users.noreply.github.com>
2022-02-16 14:29:19 +0800

    simple REST
    implement a simple REST api:
    1) GET requests
    2) POST requests
    3) PUT requests
    4) DELETE requests
    
    and that's the simple REST api!

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const http = require("http");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
 const HOST = "localhost";
 const PORT = 3000;
@@ -8,8 +9,68 @@ const PORT = 3000;
 // app will use express' module
 const app = express();
 
+// declaring usage of middlewares
 // tells app to use morgan
 app.use(morgan("dev"));
+// tells app to use bodyParser for JSON files
+app.use(bodyParser.json());
+
+// setting endpoint for all HTTP requests
+app.all('/dishes', (req, res, next) => {
+  res.statusCode = 200;
+  // eventually JSON
+  res.setHeader('Content-Type', 'text/plaintext');
+
+  // continue to look for additional specifications that match the /dishes endpoint
+  next();
+})
+
+app.get('/dishes', (req, res, send) => {
+  // from above, app.all passes res to HERE.
+  res.end('Will send all the dishes to you  !'); // ends handling of GET request
+})
+
+// POSTing a new dish
+app.post('/dishes', (req, res, send) => {
+  // POST carries some information in the body of the POST request as JSON data.
+  console.log(req.body);
+  // Suppose we have "name" and "description" as the data..
+  res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+
+
+app.put('/dishes', (req, res, send) => {
+  res.statusCode = 403;
+  res.end(`Error ${res.statusCode} PUT not supported for dishes`);
+})
+
+// DANGEROUS OPERATION!! Needs some kind of privilege handling
+app.delete('/dishes', (req, res, send) => {
+  res.end("Deleting ALL dishes...");
+})
+
+// /:dishId indicates a parameter to /dishes with dishId.
+app.get('/dishes/:dishId', (req, res, send) => {
+  // from above, app.all passes res to HERE.
+  res.end('Will send details of dish ' + req.params.dishId + ' to you'); // now we have a parameter, dishId.
+})
+
+// POSTing a new dish
+app.post('/dishes/:dishId', (req, res, send) => {
+  // you don't create a new dishId, but rather MODIFY it. POST is invalid.
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/' + req.params.dishId);
+})
+
+// Equivalent of updating a dish with dishId
+app.put('/dishes/:dishId', (req, res, send) => {
+  res.write('Updating the dish: ' + req.params.dishId);
+  res.end('\nWill update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+
+app.delete('/dishes/:dishId', (req, res, send) => {
+  res.end("Deleting dish with dishId: " + req.params.dishId);
+})
 
 // setting up html files from public folder
 // express.static tells express to set up static files from __dirname/public
